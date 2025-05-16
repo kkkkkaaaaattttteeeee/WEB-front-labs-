8b489b91d987eef26deff33c8ba565fb9a32bb19
Обедина Екатерина <catherine.ob@mail.ru>
2025-05-16 18:32:30 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзиню Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 450,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Бфлфджи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 780,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 650,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 2023,
+        price: 320,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 2021,
+        price: 500,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 2020,
+        price: 400,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Марк Твен',
+        name: 'Приключения Тома Сойера',
+        publicationYear: 2019,
+        price: 350,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Габриэль Гарсиа Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 2022,
+        price: 720,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джейн Остин',
+        name: 'Гордость и предубеждение',
+        publicationYear: 2018,
+        price: 380,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 2017,
+        price: 900,
+        bestseller: true,
+        inStock: false
+    }
+];
